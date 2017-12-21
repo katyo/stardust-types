d846e03517a485a49f56ec3f501aaec1b3a663ee
K <kayo@illumium.org>
2017-12-21 16:54:32 +0500

    Re-organized Future tests

--- test/future.ts
+++ test/future.ts
@@ -110,311 +110,327 @@ describe('future', () => {
         eb.end(Ok(true));
     });
 
-    it('and 1', (done) => {
-        a.and(c).end(res => {
-            deepEqual(res, Err(newError("Not a string")));
-            done();
-        }).start();
-        ea.end(Err(newError("Not a string")));
-        ec.end(Ok(12));
+    describe('and', () => {
+        it('case 1', (done) => {
+            a.and(c).end(res => {
+                deepEqual(res, Err(newError("Not a string")));
+                done();
+            }).start();
+            ea.end(Err(newError("Not a string")));
+            ec.end(Ok(12));
+        });
+        
+        it('case 2', (done) => {
+            a.and(c).end(res => {
+                deepEqual(res, Err(newError("Not a number")));
+                done();
+            }).start();
+            ea.end(Ok("A string"));
+            ec.end(Err(newError("Not a number")));
+        });
+        
+        it('case 3', (done) => {
+            a.and(c).end(res => {
+                deepEqual(res, Err(newError("Not a number")));
+                done();
+            }).start();
+            ea.end(Ok("A string"));
+            ec.end(Err(newError("Not a number")));
+        });
+        
+        it('case 4', (done) => {
+            a.and(c).end(res => {
+                deepEqual(res, Ok(12));
+                done();
+            }).start();
+            ea.end(Ok("A string"));
+            ec.end(Ok(12));
+        });
     });
 
-    it('and 2', (done) => {
-        a.and(c).end(res => {
-            deepEqual(res, Err(newError("Not a number")));
-            done();
-        }).start();
-        ea.end(Ok("A string"));
-        ec.end(Err(newError("Not a number")));
+    describe('or', () => {
+        it('case 1', (done) => {
+            b.or(d).end(res => {
+                deepEqual(res, Err("Bouncing"));
+                done();
+            }).start();
+            eb.end(Err("Unknown"));
+            ed.end(Err("Bouncing"));
+        });
+        
+        it('case 2', (done) => {
+            b.or(d).end(res => {
+                deepEqual(res, Ok(false));
+                done();
+            }).start();
+            eb.end(Ok(false));
+            ed.end(Err("Bouncing"));
+        });
+
+        it('case 3', (done) => {
+            b.or(d).end(res => {
+                deepEqual(res, Ok(true));
+                done();
+            }).start();
+            eb.end(Err("Unknown"));
+            ed.end(Ok(true));
+        });
+        
+        it('case 4', (done) => {
+            b.or(d).end(res => {
+                deepEqual(res, Ok(false));
+                done();
+            }).start();
+            eb.end(Ok(false));
+            ed.end(Ok(true));
+        });
+        
+        it('case 5', (done) => {
+            b.or(d).end(res => {
+                deepEqual(res, Ok(true));
+                done();
+            }).start();
+            eb.end(Ok(true));
+            ed.end(Ok(false));
+        });
+    });
+    
+    describe('and_then', () => {
+        it('case 1', (done) => {
+            a.and_then(a => {
+                deepEqual(a, "A string");
+                return c;
+            }).end(res => {
+                deepEqual(res, Ok(12));
+                done();
+            }).start();
+            ea.end(Ok("A string"));
+            ec.end(Ok(12));
+        });
+        
+        it('case 2', (done) => {
+            a.and_then(a => {
+                equal(a, "A string");
+                return c;
+            }).end(res => {
+                deepEqual(res, Err(newError("Not a string")));
+                done();
+            }).start();
+            ea.end(Err(newError("Not a string")));
+            ec.end(Ok(12));
+        });
     });
 
-    it('and 2', (done) => {
-        a.and(c).end(res => {
-            deepEqual(res, Err(newError("Not a number")));
-            done();
-        }).start();
-        ea.end(Ok("A string"));
-        ec.end(Err(newError("Not a number")));
+    describe('or_else', () => {
+        it('case 1', (done) => {
+            b.or_else((err) => {
+                deepEqual(err, "Not a value");
+                return d;
+            }).end(res => {
+                deepEqual(res, Ok(true));
+                done();
+            }).start();
+            eb.end(Ok(true));
+            ed.end(Ok(false));
+        });
+        
+        it('case 2', (done) => {
+            b.or_else((err) => {
+                deepEqual(err, "Not a value");
+                return d;
+            }).end(res => {
+                deepEqual(res, Ok(false));
+                done();
+            }).start();
+            eb.end(Ok(false));
+            ed.end(Ok(true));
+        });
+        
+        it('case 3', (done) => {
+            b.or_else((err) => {
+                deepEqual(err, "Not a value");
+                return d;
+            }).end(res => {
+                deepEqual(res, Ok(true));
+                done();
+            }).start();
+            eb.end(Err("Not a value"));
+            ed.end(Ok(true));
+        });
+        
+        it('case 4', (done) => {
+            b.or_else((err) => {
+                deepEqual(err, "Not a value");
+                return d;
+            }).end(res => {
+                deepEqual(res, Ok(false));
+                done();
+            }).start();
+            ed.end(Err("Not a value"));
+            eb.end(Ok(false));
+        });
     });
 
-    it('and 3', (done) => {
-        a.and(c).end(res => {
-            deepEqual(res, Ok(12));
-            done();
-        }).start();
-        ea.end(Ok("A string"));
-        ec.end(Ok(12));
+    describe('select', () => {
+        it('case 1', (done) => {
+            b.select(d).end(res => {
+                deepEqual(res, Ok(false));
+                done();
+            }).start();
+            eb.end(Ok(false));
+            ed.end(Err("Not a value"));
+        });
+        
+        it('case 2', (done) => {
+            b.select(d).end(res => {
+                deepEqual(res, Err("Not a value"));
+                done();
+            }).start();
+            ed.end(Err("Not a value"));
+            eb.end(Ok(false));
+        });
     });
 
-    it('or 1', (done) => {
-        b.or(d).end(res => {
-            deepEqual(res, Err("Bouncing"));
-            done();
-        }).start();
-        eb.end(Err("Unknown"));
-        ed.end(Err("Bouncing"));
+    describe('select_either', () => {
+        it('case 1', (done) => {
+            a.select_either(b).end(res => {
+                deepEqual(res, Ok(A("abc")));
+                done();
+            }).start();
+            ea.end(Ok("abc"));
+            eb.end(Ok(false));
+        });
+        
+        it('case 2', (done) => {
+            a.select_either(b).end(res => {
+                deepEqual(res, Ok(B(true)));
+                done();
+            }).start();
+            eb.end(Ok(true));
+            ea.end(Ok("abc"));
+        });
+        
+        it('case 3', (done) => {
+            a.select_either(b).end(res => {
+                deepEqual(res, Err(A(newError("def"))));
+                done();
+            }).start();
+            ea.end(Err(newError("def")));
+            eb.end(Ok(false));
+        });
+        
+        it('case 4', (done) => {
+            a.select_either(b).end(res => {
+                deepEqual(res, Err(B("???")));
+                done();
+            }).start();
+            eb.end(Err("???"));
+            ea.end(Ok("abc"));
+        });
     });
 
-    it('or 2', (done) => {
-        b.or(d).end(res => {
-            deepEqual(res, Ok(false));
-            done();
-        }).start();
-        eb.end(Ok(false));
-        ed.end(Err("Bouncing"));
+    describe('join', () => {
+        it('case 1', (done) => {
+            a.join(c).end(res => {
+                deepEqual(res, Ok(["abc", 123]));
+                done();
+            }).start();
+            ec.end(Ok(123));
+            ea.end(Ok("abc"));
+        });
+        
+        it('case 2', (done) => {
+            a.join(c).end(res => {
+                deepEqual(res, Err(newError("def")));
+                done();
+            }).start();
+            ea.end(Err(newError("def")));
+            ec.end(Ok(123));
+        });
+        
+        it('case 3', (done) => {
+            a.join(c).end(res => {
+                deepEqual(res, Err(newError("xyz")));
+                done();
+            }).start();
+            ea.end(Ok("def"));
+            ec.end(Err(newError("xyz")));
+        });
+        
+        it('case 4', (done) => {
+            a.join(c).end(res => {
+                deepEqual(res, Err(newError("xyz")));
+                done();
+            }).start();
+            ec.end(Err(newError("xyz")));
+            ea.end(Ok("def"));
+        });
     });
 
-    it('or 3', (done) => {
-        b.or(d).end(res => {
-            deepEqual(res, Ok(true));
-            done();
-        }).start();
-        eb.end(Err("Unknown"));
-        ed.end(Ok(true));
-    });
+    describe('join_all', () => {
+        it('case 1', (done) => {
+            join_all([
+                a,
+                b.map(a => `${a}`).map_err(e => newError(e)),
+                c.map(a => `${a}`),
+                d.map(a => `${!a}`).map_err(e => newError(e)),
+            ]).end(res => {
+                deepEqual(res, Ok(["xyz", "false", "123", "false"]));
+                done();
+            }).start();
+            ec.end(Ok(123));
+            ea.end(Ok("xyz"));
+            eb.end(Ok(false));
+            ed.end(Ok(true));
+        });
 
-    it('or 4', (done) => {
-        b.or(d).end(res => {
-            deepEqual(res, Ok(false));
-            done();
-        }).start();
-        eb.end(Ok(false));
-        ed.end(Ok(true));
-    });
-
-    it('or 5', (done) => {
-        b.or(d).end(res => {
-            deepEqual(res, Ok(true));
-            done();
-        }).start();
-        eb.end(Ok(true));
-        ed.end(Ok(false));
-    });
-
-    it('and then 1', (done) => {
-        a.and_then(a => {
-            deepEqual(a, "A string");
-            return c;
-        }).end(res => {
-            deepEqual(res, Ok(12));
-            done();
-        }).start();
-        ea.end(Ok("A string"));
-        ec.end(Ok(12));
-    });
-
-    it('and then 2', (done) => {
-        a.and_then(a => {
-            equal(a, "A string");
-            return c;
-        }).end(res => {
-            deepEqual(res, Err(newError("Not a string")));
-            done();
-        }).start();
-        ea.end(Err(newError("Not a string")));
-        ec.end(Ok(12));
-    });
-
-    it('or_else 1', (done) => {
-        b.or_else((err) => {
-            deepEqual(err, "Not a value");
-            return d;
-        }).end(res => {
-            deepEqual(res, Ok(true));
-            done();
-        }).start();
-        eb.end(Ok(true));
-        ed.end(Ok(false));
-    });
-
-    it('or_else 2', (done) => {
-        b.or_else((err) => {
-            deepEqual(err, "Not a value");
-            return d;
-        }).end(res => {
-            deepEqual(res, Ok(false));
-            done();
-        }).start();
-        eb.end(Ok(false));
-        ed.end(Ok(true));
-    });
-
-    it('or_else 3', (done) => {
-        b.or_else((err) => {
-            deepEqual(err, "Not a value");
-            return d;
-        }).end(res => {
-            deepEqual(res, Ok(true));
-            done();
-        }).start();
-        eb.end(Err("Not a value"));
-        ed.end(Ok(true));
-    });
-
-    it('or_else 4', (done) => {
-        b.or_else((err) => {
-            deepEqual(err, "Not a value");
-            return d;
-        }).end(res => {
-            deepEqual(res, Ok(false));
-            done();
-        }).start();
-        ed.end(Err("Not a value"));
-        eb.end(Ok(false));
-    });
-
-    it('select 1', (done) => {
-        b.select(d).end(res => {
-            deepEqual(res, Ok(false));
-            done();
-        }).start();
-        eb.end(Ok(false));
-        ed.end(Err("Not a value"));
-    });
-
-    it('select 2', (done) => {
-        b.select(d).end(res => {
-            deepEqual(res, Err("Not a value"));
-            done();
-        }).start();
-        ed.end(Err("Not a value"));
-        eb.end(Ok(false));
-    });
-
-    it('select_either 1', (done) => {
-        a.select_either(b).end(res => {
-            deepEqual(res, Ok(A("abc")));
-            done();
-        }).start();
-        ea.end(Ok("abc"));
-        eb.end(Ok(false));
-    });
-
-    it('select_either 2', (done) => {
-        a.select_either(b).end(res => {
-            deepEqual(res, Ok(B(true)));
-            done();
-        }).start();
-        eb.end(Ok(true));
-        ea.end(Ok("abc"));
-    });
-
-    it('select_either 3', (done) => {
-        a.select_either(b).end(res => {
-            deepEqual(res, Err(A(newError("def"))));
-            done();
-        }).start();
-        ea.end(Err(newError("def")));
-        eb.end(Ok(false));
-    });
-
-    it('select_either 4', (done) => {
-        a.select_either(b).end(res => {
-            deepEqual(res, Err(B("???")));
-            done();
-        }).start();
-        eb.end(Err("???"));
-        ea.end(Ok("abc"));
-    });
-
-    it('join 1', (done) => {
-        a.join(c).end(res => {
-            deepEqual(res, Ok(["abc", 123]));
-            done();
-        }).start();
-        ec.end(Ok(123));
-        ea.end(Ok("abc"));
-    });
-
-    it('join 2', (done) => {
-        a.join(c).end(res => {
-            deepEqual(res, Err(newError("def")));
-            done();
-        }).start();
-        ea.end(Err(newError("def")));
-        ec.end(Ok(123));
-    });
-
-    it('join 3', (done) => {
-        a.join(c).end(res => {
-            deepEqual(res, Err(newError("xyz")));
-            done();
-        }).start();
-        ea.end(Ok("def"));
-        ec.end(Err(newError("xyz")));
-    });
-
-    it('join 4', (done) => {
-        a.join(c).end(res => {
-            deepEqual(res, Err(newError("xyz")));
-            done();
-        }).start();
-        ec.end(Err(newError("xyz")));
-        ea.end(Ok("def"));
-    });
-
-    it('join_all 1', (done) => {
-        join_all([
-            a,
-            b.map(a => `${a}`).map_err(e => newError(e)),
-            c.map(a => `${a}`),
-            d.map(a => `${!a}`).map_err(e => newError(e)),
-        ]).end(res => {
-            deepEqual(res, Ok(["xyz", "false", "123", "false"]));
-            done();
-        }).start();
-        ec.end(Ok(123));
-        ea.end(Ok("xyz"));
-        eb.end(Ok(false));
-        ed.end(Ok(true));
-    });
-
-    it('join_all 2', (done) => {
-        join_all([
-            a,
-            b.map(a => `${a}`).map_err(e => newError(e)),
-            c.map(a => `${a}`),
-            d.map(a => `${!a}`).map_err(e => newError(e)),
-        ]).end(res => {
-            deepEqual(res, Err(newError("hmm...")));
-            done();
-        }).start();
-        ec.end(Ok(123));
-        ea.end(Ok("xyz"));
-        eb.end(Err("hmm..."));
-        ed.end(Ok(true));
-    });
-
-    it('join_all 3', (done) => {
-        join_all([
-            a,
-            b.map(a => `${a}`).map_err(e => newError(e)),
-            c.map(a => `${a}`),
-            d.map(a => `${!a}`).map_err(e => newError(e)),
-        ]).end(res => {
-            deepEqual(res, Err(newError("Something went wrong")));
-            done();
-        }).start();
-        ec.end(Ok(123));
-        ea.end(Err(newError("Something went wrong")));
-        eb.end(Err("hmm..."));
-        ed.end(Ok(true));
-    });
-
-    it('join_all 4', (done) => {
-        join_all([
-            a,
-            b.map(a => `${a}`).map_err(e => newError(e)),
-            c.map(a => `${a}`),
-            d.map(a => `${!a}`).map_err(e => newError(e)),
-        ]).end(res => {
-            deepEqual(res, Err(newError("Not a number")));
-            done();
-        }).start();
-        ec.end(Err(newError("Not a number")));
-        ea.end(Err(newError("Something went wrong")));
-        eb.end(Err("hmm..."));
-        ed.end(Ok(true));
+        it('case 2', (done) => {
+            join_all([
+                a,
+                b.map(a => `${a}`).map_err(e => newError(e)),
+                c.map(a => `${a}`),
+                d.map(a => `${!a}`).map_err(e => newError(e)),
+            ]).end(res => {
+                deepEqual(res, Err(newError("hmm...")));
+                done();
+            }).start();
+            ec.end(Ok(123));
+            ea.end(Ok("xyz"));
+            eb.end(Err("hmm..."));
+            ed.end(Ok(true));
+        });
+        
+        it('case 3', (done) => {
+            join_all([
+                a,
+                b.map(a => `${a}`).map_err(e => newError(e)),
+                c.map(a => `${a}`),
+                d.map(a => `${!a}`).map_err(e => newError(e)),
+            ]).end(res => {
+                deepEqual(res, Err(newError("Something went wrong")));
+                done();
+            }).start();
+            ec.end(Ok(123));
+            ea.end(Err(newError("Something went wrong")));
+            eb.end(Err("hmm..."));
+            ed.end(Ok(true));
+        });
+        
+        it('case 4', (done) => {
+            join_all([
+                a,
+                b.map(a => `${a}`).map_err(e => newError(e)),
+                c.map(a => `${a}`),
+                d.map(a => `${!a}`).map_err(e => newError(e)),
+            ]).end(res => {
+                deepEqual(res, Err(newError("Not a number")));
+                done();
+            }).start();
+            ec.end(Err(newError("Not a number")));
+            ea.end(Err(newError("Something went wrong")));
+            eb.end(Err("hmm..."));
+            ed.end(Ok(true));
+        });
     });
 
     describe('loop', () => {
